34ac23c1048332ecd03ae2eaed85530c4bd45811
KubilayKarayilan <kkarayilan@yahoo.no>
2015-01-16 20:26:51 +0100

    echo change client js websocket to ws://localhost:8080/inbound

--- src/main/java/no/inmeta/iwsocket/IServerEndPoint.java
+++ src/main/java/no/inmeta/iwsocket/IServerEndPoint.java
@@ -1,19 +1,21 @@
 package no.inmeta.iwsocket;
 
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 /**
  * Created by K on 1/16/15.
  */
 @ServerEndpoint("/inbound")
 public class IServerEndPoint {
-
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("opened");
-
-
+        session.getUserProperties().put("chatroom","chatroom");
 
     }
     @OnClose
@@ -21,7 +23,18 @@ public class IServerEndPoint {
 
     }
     @OnMessage
-    public String onMessaege(String string,Session session) {
-        System.out.println(  string + " recieved sending to client This is from server");return string;
+    public void onMessaege(String string,Session session) {
+        System.out.println(  string + " recieved sending to client This is from server size : "  );
+        String room = (String) session.getUserProperties().get("chatroom");
+        try {
+            for (Session s : session.getOpenSessions()) {
+                if (s.isOpen()
+                        && room.equals(s.getUserProperties().get("chatroom"))) {
+                    s.getBasicRemote().sendText(string);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
    }
 }
--- src/main/webapp/webAppJs/client.js
+++ src/main/webapp/webAppJs/client.js
@@ -2,24 +2,20 @@
 
 function sendMessage(){
     var txt2snd=$("#messageText").val();
-    console.log("*********** sending message "+ txt2snd);
-    var ws = new WebSocket("ws://localhost:8080/inbound");
+    var ws = new WebSocket("ws://localhost:8080/target/inbound");
     ws.onopen = function()
     {
         // Web Socket is connected, send data using send()
         ws.send(txt2snd);
-        alert("Message is sent..." + txt2snd);
     };
     ws.onmessage = function (evt)
     {
         var received_msg = evt.data;
-        alert("Message is received..."+ received_msg);
-        $("#messageLog").val(received_msg);
+        $("#messageLog").append(received_msg + "\n");
     };
     ws.onclose = function()
     {
         // websocket is closed.
-        alert("Connection is closed...");
     };
 
 
