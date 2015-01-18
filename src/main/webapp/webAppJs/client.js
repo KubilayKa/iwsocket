9feeddcaf6610a21f21655f4dcde7463c3675823
KubilayKarayilan <kkarayilan@yahoo.no>
2015-01-18 22:47:09 +0100

    html

--- src/main/webapp/webAppJs/client.js
+++ src/main/webapp/webAppJs/client.js
@@ -6,7 +6,13 @@ function screenResize() {
     $('#pictureHolder').css( {"width":width-20,"height":height-50})
     $('#player1').css( {"width":((width-50)/2),"height":( height-50)  })
     $('#player2').css( {"width":((width-50)/2),"height":( height-50) })
-    $('#vs').css( {"width":( width/6),"height":( height/3),"left": (width/2) -(width/10),"top":50,"backgroundImage":"url(img/vs1.png)","backgroundSize":"100%"})
+    $('#vs').css( {"width":( width/6),"height":( height/3),
+        "left": (width/2) -(width/10),"top":(height/10)})  ;
+    $('#table').css( {"width":( width/4),"height":( height/3),
+        "left": (width/2) -(width/7),"top":(height/2)})  ;
+    $('#vsImage').css( {"width":"100%","height":"100%"})
+    $('.playerImg').css({"width":(width-50)/4,"height":(height/2),"marginLeft":(width-50)/9})
+    var counter= $('.counter').FlipClock(200,{clockFace:'MinuteCounter',countdown:"true"});
 
 }
 function sendMessage(){
