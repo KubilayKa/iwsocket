e62d65d44190acd3edb615bdf817817e861a3727
kubkaray <Kubilay.Karayilan@inmeta.no>
2015-01-21 14:14:47 +0100

    Works

--- src/main/webapp/webAppJs/client.js
+++ src/main/webapp/webAppJs/client.js
@@ -1,4 +1,5 @@
-{
+{  var height= $(document).height();
+    var width= $(document).width();
     var counter;
     var counterL;
     var counterR;
@@ -20,7 +21,16 @@
             console.log("Dec");
             counterL.decrement();
             console.log(received_msg);
-        }else {
+        }else if("stats" == received_msg){
+            if($('#leftDrawer').width() > 10){
+
+                $('#leftDrawer').css({"width":((width-50)/2),"height":( height-50)  }).animate({width:'0px',visibility:"hidden" })
+                console.log("visible")
+            }else {
+
+                $('#leftDrawer').css({"width":((width-50)/2),"height":( height-50),"visibility":"visible"  }).animate({width:width/2})
+                console.log("not visible")
+            }
             console.log(received_msg);
         }
         $("#messageLog").append(received_msg + "\n");
@@ -31,9 +41,7 @@
     };
 
     function screenResize() {
-   var height= $(document).height();
-   var width= $(document).width();
-    console.log("size " + height + " : "+width);
+
     $('#pictureHolder').css( {"width":width-20,"height":height-50})
     $('#player1').css( {"width":((width-50)/2),"height":( height-50)  })
     $('#player2').css( {"width":((width-50)/2),"height":( height-50) })
@@ -45,9 +53,10 @@
     $('#counterTdL').css({"width":width/3 });
     $('#counterTdC').css({"width":width/3});
     $('#counterTdR').css({"width":width/3 });
-      counter= $('.counter').FlipClock(000,{clockFace:'MinuteCounter',countdown:"true"});
 
-  counterL= $('.counterL').FlipClock({clockFace:'Counter' });
+
+     counter= $('.counter').FlipClock(000,{clockFace:'MinuteCounter',countdown:"true"});
+     counterL= $('.counterL').FlipClock({clockFace:'Counter' });
      counterR= $('.counterR').FlipClock(000,{clockFace:'Counter' });
 
 
