fc04d2638913b646868b3c4ba57672a61492fdcb
KubilayKarayilan <kkarayilan@yahoo.no>
2015-02-22 19:50:32 +0100

    mrapport

--- src/main/webapp/webAppJs/mraport.js
+++ src/main/webapp/webAppJs/mraport.js
@@ -1,4 +1,6 @@
+var ofset = $("#energyBall").offset();
 var ws = new WebSocket("ws://localhost:8080/iwsocket/minbound");
+
 ws.onopen = function () {
     // Web Socket is connected, send data using send()
 
@@ -7,6 +9,18 @@ ws.onmessage = function (evt) {
   var xyz=evt.data.split(":");
 
     console.log(xyz);
+    var oleft=ofset.left ;
+    var otop=ofset.top  ;
+    if(xyz[0]<2&&-2>xyz[0]){
+        $("#energyBall").animate({left:oleft,top:otop},"fast")
+    }else {
+        if(xyz[0]>2) {
+            $("#energyBall").animate({left:oleft-(xyz[0]/5),top:otop},"fast")
+        }else if(xyz[0]< -2) {
+            $("#energyBall").animate({left:oleft-(xyz[0]/5),top:otop},"fast")
+
+        }
+    }
     console.log($("#energyBall").offset());
 };
 ws.onclose = function () {
