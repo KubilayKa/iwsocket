14e52ef970e13f2b0bc51ebbc0782508f0ca310c
KubilayKarayilan <kkarayilan@yahoo.no>
2015-02-22 18:46:25 +0100

    mrapport

---
+++ src/main/java/no/inmeta/iwsocket/MServerEndpoint.java
@@ -0,0 +1,41 @@
+package no.inmeta.iwsocket;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Created by K on 2/22/15.
+ */
+@ServerEndpoint(value = "/minbound")
+public class MServerEndpoint {
+
+
+        @OnOpen
+        public void onOpen(Session session) throws IOException {
+            //  session.getUserProperties().put("chatroom","chatroom");
+            Map<String, String> pathPrm = session.getPathParameters();
+           // String firstPlayer = pathPrm.get("first");
+            //String secondPlayer = pathPrm.get("second");
+            //String roomId = firstPlayer + "&" + secondPlayer;
+            //String sessionId = session.getId();
+
+            session.getUserProperties().put("roomName", "main");
+
+        }
+
+        @OnClose
+        public void onClose(Session session)  {
+
+        }
+
+        @OnMessage
+        public void onMessaege(String string, Session session) throws IOException {
+
+        }
+    }
+
